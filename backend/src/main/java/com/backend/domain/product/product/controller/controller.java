63e40cb1213100a4c5cb25219324615bb51bbe4e
zwhstn-boop <zwhstn@gmail.com>
2026-06-05 11:06:37 +0900

    feat/컨트롤러 추ê가#21

---
+++ backend/src/main/java/com/backend/domain/product/product/controller/controller.java
@@ -0,0 +1,15 @@
+package com.backend.domain.product.product.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+
+}
