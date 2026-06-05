2c35d0de8da986af4a0c891c6a20ea05bac631fd
BeomBeom2 <whwjdqja20@naver.com>
2026-06-05 11:55:37 +0900

    [Feat] 컨트롤러 테스트 세팅

---
+++ backend/src/test/java/com/backend/domain.product.product.controller/ApiV1ProductControllerTest.java
@@ -0,0 +1,20 @@
+package com.backend.domain.product.product.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ApiV1ProductControllerTest {
+    @Autowired
+    private MockMvc mvc;
+//    @Autowired
+//    private ProductService productService;
+
+}
