8bac076470a11d77abafaaaba16e66ea16d56d3f
leesm-225 <lshine12393@gmail.com>
2026-06-05 10:49:06 +0900

    admin-p-service

--- backend/src/main/java/com/backend/domain/order/order/service/OrderService.java
+++ backend/src/main/java/com/backend/domain/order/order/service/OrderService.java
@@ -1,4 +1,9 @@
 package com.backend.domain.order.order.service;
 
+import com.backend.domain.order.order.repository.OrderRepository;
+
 public class OrderService {
+    private OrderRepository orderRepository;
+
+
 }
--- backend/src/main/java/com/backend/domain/product/product/service/ProductService.java
+++ backend/src/main/java/com/backend/domain/product/product/service/ProductService.java
@@ -1,13 +1,25 @@
 package com.backend.domain.product.product.service;
 
 import com.backend.domain.product.product.entity.Product;
+import com.backend.domain.product.product.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
-    public Optional<Product> findById(Long id){
-        return productRepository.findById(id);
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("상품을 찾을 수 없습니다: " + id));
     }
 }
